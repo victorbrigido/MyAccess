16c81d428fc0c3c41bbcd71ab9954ca360da8240
Victor Brigido <victorbrigido@icloud.com>
2024-03-11 11:34:13 -0300

    Initial Commit

---
+++ MyAcess/MyAcessApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAcessApp.swift
+//  MyAcess
+//
+//  Created by Victor Brigido on 11/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MyAcessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
